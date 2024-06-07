8f7fa4d13fa28672e45e0403b9d21b0d18796a9c
Harsha <ha>
2024-06-07 12:44:51 -0700

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Harshavardhan Tadiparthi on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
